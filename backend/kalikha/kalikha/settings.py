91f352e7d87e1178db5c46fba1379ec24d02d9d5
perimeterr <mikaela.paderna@student.ateneo.edu>
2025-04-06 19:34:42 +0800

    Hid secret key using os

--- backend/kalikha/kalikha/settings.py
+++ backend/kalikha/kalikha/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-44nk9$gw99w45$45!8qhfik8rq@x_@3#!^bfsgu_36e-dt1aju'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
