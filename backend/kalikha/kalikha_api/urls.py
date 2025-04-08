67875ec5272b42ac1706a0e18b914950f118d74e
macasalad <hannahleibaniqued12@gmail.com>
2025-04-09 04:56:59 +0800

    Added basic urls

--- backend/kalikha/kalikha_api/urls.py
+++ backend/kalikha/kalikha_api/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.home, name='home'),
+    path('home/', views.home, name='home'),
+    path('signUp/', views.sign_up, name='sign_up'),
+    path('signIn/', views.sign_in, name='sign_in'),
+    path('signOut/', views.sign_out, name='sign_out'),
 ]
 
 
