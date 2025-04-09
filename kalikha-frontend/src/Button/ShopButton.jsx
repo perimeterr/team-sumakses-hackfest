05f6f3a81ca1f39c645aa814b07012b0b290ad56
pris-matic <deocadizjranthony@gmail.com>
2025-04-09 12:37:14 +0800

    updated button

--- kalikha-frontend/src/Button/ShopButton.jsx
+++ kalikha-frontend/src/Button/ShopButton.jsx
@@ -1,11 +1,21 @@
 import styles from './Button.module.css';
 import { FaArrowRight } from 'react-icons/fa';
+import { useNavigate } from 'react-router-dom';
+
 
 function ShopButton() {
+    const navigate = useNavigate();
 
-    return(
-        <button className={styles.shopButton}>
-            <span className={styles.shopText}>SHOP UPCYCLABLES <FaArrowRight /></span>
+    const handleClick = () => {
+        window.scrollTo({ top: 0});
+        navigate('/listings');
+    };
+
+    return (
+        <button onClick={handleClick} className={styles.shopButton}>
+            <span className={styles.shopText}>
+                SHOP UPCYCLABLES <FaArrowRight />
+            </span>
         </button>
     );
 }
