a19267b0546704606f20ca8ef5d97f34bda3c869
macasalad <hannahleibaniqued12@gmail.com>
2025-04-09 13:18:35 +0800

    Added Fixed urls.py

--- backend/kalikha/kalikha_api/urls.py
+++ backend/kalikha/kalikha_api/urls.py
@@ -1,13 +1,16 @@
 from django.urls import path
 from . import views
 
+app_name = 'kalikha_api'
+
 urlpatterns = [
     path('', views.home, name='home'),
     path('home/', views.home, name='home'),
-    path('signUp/', views.sign_up, name='sign_up'),
-    path('signIn/', views.sign_in, name='sign_in'),
-    path('signOut/', views.sign_out, name='sign_out'),
+    path('signup/', views.sign_up, name='sign_up'),
+    path('signin/', views.sign_in, name='sign_in'),
+    path('signout/', views.sign_out, name='sign_out'),
+    path('resources/', views.resource_listing_list, name='resource_listing_list'),
+    path('resources/create/', views.create_resource_listing, name='create_resource_listing'),
+    path('resources/<int:pk>/', views.resource_listing_detail, name='resource_listing_detail'),
+    path('resources/<int:listing_pk>/add-material/', views.add_material_to_listing, name='add_material_to_listing'),
 ]
-
-
-app_name = 'kalikha_api'
